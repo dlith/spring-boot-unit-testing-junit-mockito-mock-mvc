957af6ac937fe2efbac03ab8025854b5a8a2ca9f
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-11-30 13:46:03 +0100

    addind test case lifecycle methods

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,14 +1,37 @@
 package com.luv2code.junitdemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
 
+    private DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach(){
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach executes before the execution of each test method");
+    }
+
+    @AfterEach
+    void tearDownAfterEach(){
+        System.out.println("Running @AfterEach\n");
+    }
+
+    @BeforeAll
+    static void setupBeforeEachClass(){
+        System.out.println("@BeforeAll executes only once before all test methods execution in the class\n");
+    }
+
+    @AfterAll
+    static void tearDownAfterAll(){
+        System.out.println("@AfterAll executes only once after all test methods execution in the class");
+    }
+
     @Test
     void testEqualsAndNotEquals(){
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testEqualsAndNotEquals");
 
         assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1,9), "1+9 must not be 6");
@@ -16,7 +39,7 @@ class DemoUtilsTest {
 
     @Test
     void testNullAndNotNull(){
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testNullAndNotNull");
 
         String str1 = null;
         String str2 = "test";
