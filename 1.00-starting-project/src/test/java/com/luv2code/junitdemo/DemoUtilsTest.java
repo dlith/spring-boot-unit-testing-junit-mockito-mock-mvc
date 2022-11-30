bcce15008e6b730e3811090a5e153dbdb6d9eff6
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-11-30 12:28:23 +0100

    adding equals/not equals and null/notNull tests

---
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -0,0 +1,27 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoUtilsTest {
+
+    @Test
+    void testEqualsAndNotEquals(){
+        DemoUtils demoUtils = new DemoUtils();
+
+        assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
+        assertNotEquals(6, demoUtils.add(1,9), "1+9 must not be 6");
+    }
+
+    @Test
+    void testNullAndNotNull(){
+        DemoUtils demoUtils = new DemoUtils();
+
+        String str1 = null;
+        String str2 = "test";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+    }
+}
