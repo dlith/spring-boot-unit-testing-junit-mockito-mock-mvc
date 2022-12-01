939d8c27221a67f2b6d971119e3fb60366503245
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-12-01 15:42:44 +0100

    adding annotation @DisplayName

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -14,6 +14,27 @@ class DemoUtilsTest {
         System.out.println("@BeforeEach executes before the execution of each test method");
     }
 
+    @Test
+    @DisplayName("Equals and Not Equals")
+    void testEqualsAndNotEquals(){
+        System.out.println("Running test: testEqualsAndNotEquals");
+
+        assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
+        assertNotEquals(6, demoUtils.add(1,9), "1+9 must not be 6");
+    }
+
+    @Test
+    @DisplayName("Null and Not Null")
+    void testNullAndNotNull(){
+        System.out.println("Running test: testNullAndNotNull");
+
+        String str1 = null;
+        String str2 = "test";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+    }
+
     @AfterEach
     void tearDownAfterEach(){
         System.out.println("Running @AfterEach\n");
@@ -27,24 +48,5 @@ class DemoUtilsTest {
     @AfterAll
     static void tearDownAfterAll(){
         System.out.println("@AfterAll executes only once after all test methods execution in the class");
-    }
-
-    @Test
-    void testEqualsAndNotEquals(){
-        System.out.println("Running test: testEqualsAndNotEquals");
-
-        assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
-        assertNotEquals(6, demoUtils.add(1,9), "1+9 must not be 6");
-    }
-
-    @Test
-    void testNullAndNotNull(){
-        System.out.println("Running test: testNullAndNotNull");
-
-        String str1 = null;
-        String str2 = "test";
-
-        assertNull(demoUtils.checkNull(str1), "Object should be null");
-        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
     }
 }
