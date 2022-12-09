0c918a65e26629ab526255cf6c1a4f563bdc78d7
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-12-09 20:18:35 +0100

    adding dependency and empty test

---
+++ 2.00-starting-project/src/test/java/com/luv2code/component/ApplicationExampleTest.java
@@ -0,0 +1,13 @@
+package com.luv2code.component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+class ApplicationExampleTest {
+
+    @Test
+    void basicTest(){
+
+    }
+}
