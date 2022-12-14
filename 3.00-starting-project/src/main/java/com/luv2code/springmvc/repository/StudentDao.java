f5697c0402a4497d7cb1d9ee175708827439b9ec
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-12-14 19:05:43 +0100

    updating the test to pass

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/repository/StudentDao.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/repository/StudentDao.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentDao extends CrudRepository<CollegeStudent, Integer> {
+
+    public CollegeStudent findByEmailAddress(String emailAddress);
 }
--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -1,5 +1,7 @@
 package com.luv2code.springmvc;
 
+import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.repository.StudentDao;
 import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +16,15 @@ class StudentAndGradeServiceTest {
 
     @Autowired
     private StudentAndGradeService studentService;
+    @Autowired
+    private StudentDao studentDao;
 
     @Test
     void createStudentService(){
 
         studentService.createStudent("Chad", "Darby", "chad.darby@gmail.com");
-//        CollegeStudent student = StudentDao.findByEmailAddress("chad.darby@gmail.com");
-//        assertEquals("chad.darby@gmail.com", student.getEmailAddress(), "find by email");
+        CollegeStudent student = studentDao.findByEmailAddress("chad.darby@gmail.com");
+        assertEquals("chad.darby@gmail.com", student.getEmailAddress(), "find by email");
     }
 
 }
