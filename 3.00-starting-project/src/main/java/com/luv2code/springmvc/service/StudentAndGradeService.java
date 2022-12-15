15080fc89ed7011fe9e4f624a32400eb0f499a1f
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-12-15 13:10:05 +0100

    adding integration tests

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 public class StudentAndGradeService {
@@ -17,5 +19,16 @@ public class StudentAndGradeService {
         CollegeStudent student = new CollegeStudent(firstName, lastName, emailAddress);
         student.setId(0);
         studentDao.save(student);
+    }
+
+    public boolean checkIfStudentIsNull(int id) {
+        Optional<CollegeStudent> student = studentDao.findById(id);
+        return student.isPresent();
+    }
+
+    public void deleteStudent(int id) {
+        if(checkIfStudentIsNull(id)){
+            studentDao.deleteById(id);
+        }
     }
 }
--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -3,12 +3,19 @@ package com.luv2code.springmvc;
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.repository.StudentDao;
 import com.luv2code.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Optional;
+import java.util.OptionalInt;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestPropertySource("/application.properties")
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
@@ -19,6 +26,15 @@ class StudentAndGradeServiceTest {
     @Autowired
     private StudentDao studentDao;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setupDB(){
+        jdbcTemplate.execute("insert into student(id, firstname, lastname, email_address) " +
+                "values (1, 'Eric', 'Roby', 'eric.roby@gmail.com')");
+    }
+
     @Test
     void createStudentService(){
 
@@ -27,4 +43,25 @@ class StudentAndGradeServiceTest {
         assertEquals("chad.darby@gmail.com", student.getEmailAddress(), "find by email");
     }
 
+    @Test
+    void isStudentNullCheck(){
+        assertTrue(studentService.checkIfStudentIsNull(1));
+        assertFalse(studentService.checkIfStudentIsNull(0));
+    }
+
+    @Test
+    void deleteStudentService(){
+        Optional<CollegeStudent> deletedCollegeStudent = studentDao.findById(1);
+
+        assertTrue(deletedCollegeStudent.isPresent(), "Return true");
+        studentService.deleteStudent(1);
+
+        deletedCollegeStudent = studentDao.findById(1);
+        assertFalse(deletedCollegeStudent.isPresent(), "Return false");
+    }
+
+    @AfterEach
+    void setupAfterTransaction(){
+        jdbcTemplate.execute("delete from student");
+    }
 }
