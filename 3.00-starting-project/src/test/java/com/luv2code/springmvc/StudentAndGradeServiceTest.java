167b59b03fd645fe3fed501d58c521688dd0ab89
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-01-01 13:17:51 +0100

    Refactor Grade Service for Collections

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -19,6 +19,7 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -117,9 +118,9 @@ class StudentAndGradeServiceTest {
         Iterable<ScienceGrade> scienceGrades = scienceGradeDao.findGradeByStudentId(1);
         Iterable<HistoryGrade> historyGrades = historyGradeDao.findGradeByStudentId(1);
 
-        assertTrue(mathGrades.iterator().hasNext(), "Student has math grades");
-        assertTrue(scienceGrades.iterator().hasNext(), "Student has science grades");
-        assertTrue(historyGrades.iterator().hasNext(), "Student has history grades");
+        assertTrue(((Collection<MathGrade>) mathGrades).size() == 2, "Student has math grades");
+        assertTrue(((Collection<ScienceGrade>) scienceGrades).size() == 2, "Student has science grades");
+        assertTrue(((Collection<HistoryGrade>) historyGrades).size() == 2, "Student has history grades");
     }
 
     @Test
