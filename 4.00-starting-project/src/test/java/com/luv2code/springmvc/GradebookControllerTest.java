813e625991d208fb92c3c701a4a9cb2ad878c929
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-01-08 09:32:15 +0100

    Create Grade Test

--- 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 4.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -191,6 +191,23 @@ public class GradebookControllerTest {
                 .andExpect(jsonPath("$.message", is("Student or Grade was not found")));
     }
 
+    @Test
+    void createAValidGradeHttpRequest() throws Exception {
+        this.mockMvc.perform(post("/grades")
+                .contentType(APPLICATION_JSON_UTF_8)
+                .param("grade", "85.00")
+                .param("gradeType", "math")
+                .param("studentId", "1"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(APPLICATION_JSON_UTF_8))
+        .andExpect(jsonPath("$.id", is(1)))
+        .andExpect(jsonPath("$.firstname", is("Eric")))
+        .andExpect(jsonPath("$.lastname", is("Roby")))
+        .andExpect(jsonPath("$.emailAddress", is("eric.roby@luv2code_school.com")))
+        .andExpect(jsonPath("$.studentGrades.mathGradeResults", hasSize(2)));
+
+    }
+
     @AfterEach
     public void setupAfterTransaction() {
         jdbc.execute(sqlDeleteStudent);
